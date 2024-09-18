3be080a114eda93c6c72912ca466f6749d0a741f
Ded0Ak <davidkagricenok@gmail.com>
2024-09-18 19:43:51 +0300

    Laboratory 1.5

--- labs/1.5/main.cpp
+++ labs/1.5/main.cpp
@@ -1,14 +1,52 @@
 /*
-* Done by:
-* Student Name: David Hrytsenok
-* Student Group: 121
-* Lab 1.5 
-*/
+ * Done by:
+ * Student Name: David Hrytsenok
+ * Student Group: B-121-24-1-SE
+ * Lab 1.5
+ */
 
 #include <iostream>
-using namespace std
+#include <cmath>
 
-int main() {
+using namespace std;
+
+int main()
+{
+    cout << "Part I" << endl;
+    {
+        float a = 5.8,
+              b = 39.1;
+        int c = 70,
+            d = 42;
+
+        bool res = (!(a = b) ^ (-(c < d)));
+
+        cout << "Result: " << boolalpha << res << endl;
+    }
+
+    {
+        int a = 85,
+            b = 85;
+        float c = 6.4,
+              d = 9.3;
+
+        bool res = (!(a = b) ^ (-(c < d)));
+        cout << "Result: " << boolalpha << res << endl;
+    }
+    cout << "Part II" << endl;
+
+    {
+        const int A = 49;
+        int B = -65,
+            C = 23,
+            D = 13,
+            E = 2;
+        const char *F = "long";
+
+        long result = (A ^ (-B)) + (C * D) - (E << sizeof(F));
+
+        cout << "Result: " << result << endl;
+    }
 
     return 0;
 }
